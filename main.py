9bbd7e32d9732ba86914893805fa4a3aef6b938e
Kevin Mattix <kmattrider@gmail.com>
2022-10-13 15:09:27 -0400

    removes print_function (not used)

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from spreadsheet import Spreadsheet
 
 #Backend for accessing all university esports data from
--- spreadsheet.py
+++ spreadsheet.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from university import University
 
 import os.path
